468ac1f2f09fd34a4d9e362c1517dee6498fb137
GoudhamT <goudhamvel.88@gmail.com>
2026-01-11 08:49:11 -0800

    basic functionality

--- src/BasicNFT.sol
+++ src/BasicNFT.sol
@@ -4,13 +4,22 @@ pragma solidity ^0.8.19;
 import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
 contract BasicNFT is ERC721 {
-    constructor() ERC721("Dogie", "DG") {}
+    uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenToURI;
 
-    function mintNFT() public {}
+    constructor() ERC721("Dogie", "DG") {
+        s_tokenCounter = 0;
+    }
+
+    function mintNFT(string memory _tokenToURI) public {
+        s_tokenToURI[s_tokenCounter] = _tokenToURI;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
+    }
 
     function tokenURI(
         uint256 _tokenId
     ) public view override returns (string memory) {
-        return "";
+        return s_tokenToURI[_tokenId];
     }
 }
